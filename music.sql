f78dba37462cedee02ee58aa8f5deef6cddc8fac
Elena-Lim <leoparrrd.lim@gmail.com>
2025-03-01 15:55:31 +0400

    Update music.sql

--- music.sql
+++ music.sql
@@ -26,6 +26,24 @@ CREATE TABLE IF NOT EXISTS AlbumMusician (
 	CONSTRAINT pk_AlbumMusician PRIMARY KEY (Album_id, Musician_id)
 );
 
+CREATE TABLE IF NOT EXISTS Songs (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	duration INTEGER,
+	Album_id INTEGER REFERENCES Album(id) 
+);
+
+CREATE TABLE IF NOT EXISTS Collection (   
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	year_of INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS SongsCollection (
+	Songs_id INTEGER REFERENCES Songs(id),
+	Collection_id INTEGER REFERENCES Collection(id),
+	CONSTRAINT pk_SongsCollection PRIMARY KEY (Songs_id, Collection_id)
+);
 
 --insert
 INSERT INTO Genre
@@ -162,21 +180,3 @@ JOIN Musician e ON ea.Musician_id = e.Musician_id
 WHERE e.name LIKE 'Ramstein';
 
 
-CREATE TABLE IF NOT EXISTS Songs (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	duration INTEGER,
-	Album_id INTEGER REFERENCES Album(id) 
-);
-
-CREATE TABLE IF NOT EXISTS Collection (   
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL,
-	year_of INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS SongsCollection (
-	Songs_id INTEGER REFERENCES Songs(id),
-	Collection_id INTEGER REFERENCES Collection(id),
-	CONSTRAINT pk_SongsCollection PRIMARY KEY (Songs_id, Collection_id)
-);
